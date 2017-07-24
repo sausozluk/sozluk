a499ff9fe545474b00ee7958a20d9c151cf965f5
Eray Arslan <relfishere@gmail.com>
2017-07-24 19:52:08 +0300

    improvement

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,4 @@
 #!/usr/bin/env bash
 sudo docker rmi -f $(sudo docker images -f "dangling=true" -q)
-sudo docker-compose down
-sudo docker-compose rm
 sudo docker-compose pull
-sudo docker-compose build --no-cache
-sudo docker-compose up -d --force-recreate
+sudo docker-compose up -d --build
